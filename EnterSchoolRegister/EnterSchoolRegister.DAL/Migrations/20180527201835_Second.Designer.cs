2d68aed9b48ca1b653f81f333dc4db546de5b5e8
Vincenzo <vinc.parrilla@gmail.com>
2018-05-28 11:07:32 +0200

    Ready for Controllers, Views and (if needed) other Services

--- EnterSchoolRegister/EnterSchoolRegister.DAL/Migrations/20180527201835_Second.Designer.cs
+++ EnterSchoolRegister/EnterSchoolRegister.DAL/Migrations/20180527201835_Second.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace EnterSchoolRegister.DAL.Migrations
 {
     [DbContext(typeof(DbContext<User, Role, int>))]
-    partial class DbContextModelSnapshot : ModelSnapshot
+    [Migration("20180527201835_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
